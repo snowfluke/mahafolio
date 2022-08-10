c9c8bdf7cf4b021070ff841245c788c630588b09
Willy Setiawan <wilisetiawan29@gmail.com>
2022-08-10 23:17:02 +0700

    Create footer component

--- front/src/components/footer.jsx
+++ front/src/components/footer.jsx
@@ -1,5 +1,12 @@
 function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <footer className="text-center py-4 space-y-2 border-t">
+      <span className="font-semibold">
+        #2022 Mahafolio ~ <a href="https://github.com/snowfluke/mahafolio">Repositori GitHub</a>
+      </span>
+      <h2 className="font-semibold text-2xl">~ Portofolio Mahasiswa ~</h2>
+    </footer>
+  );
 }
 
 export default Footer;
