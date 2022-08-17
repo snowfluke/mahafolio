fb7954f3045e55ce7c0960ba416f4a45d146c455
Willy Setiawan <wilisetiawan29@gmail.com>
2022-08-18 00:23:39 +0700

    create header component

--- front/src/components/container.jsx
+++ front/src/components/container.jsx
@@ -1,5 +1,5 @@
 function Container({ children }) {
-  return <div className="w-full flex flex-col justify-between h-[100vh]">{children}</div>;
+  return <div className="w-[85%] mx-auto my-28 flex flex-col justify-between h-[100vh]">{children}</div>;
 }
 
 export default Container;
