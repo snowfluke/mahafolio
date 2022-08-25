baacbb6ffde9194a2a5be5d0950fb36bb9dab2af
snowfluke <awalariansyah7@gmail.com>
2022-08-25 22:36:16 +0700

    added sign out hooks

---
+++ front/src/hooks/useSignout.js
@@ -0,0 +1,11 @@
+import { useAuthContext } from "./useAuthContext";
+
+export const useSignout = () => {
+  const [_, { setAuthContextLoggedOut }] = useAuthContext();
+
+  const logout = () => {
+    localStorage.removeItem("mhs");
+    setAuthContextLoggedOut(response);
+  };
+  return { logout };
+};
