4c3a4126e524eb26dff6fd5ac6c2be75c6ec422a
Willy Setiawan <wilisetiawan29@gmail.com>
2022-08-26 09:39:04 +0700

    Custom font and add favicon

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        nunito: ["Nunito"],
+      },
       colors: {
         green: "#03989E",
         gray: "#F5F5F5",
