57913afe90507d0d1630de70f4862a8f1ad762a5
Willy Setiawan <wilisetiawan29@gmail.com>
2022-08-28 00:46:41 +0700

    style dropdown

--- front/src/components/form/dropdown.jsx
+++ front/src/components/form/dropdown.jsx
@@ -1,7 +1,7 @@
 function Dropdown({ items, ref, selected = "", onChange = () => false }) {
   return (
     <div class="dropdown relative">
-      <select ref={ref} onChange={onChange}>
+      <select className="px-4 py-2 shadow rounded-md block w-full" ref={ref} onChange={onChange}>
         <For each={items}>
           {(item) => (
             <option selected={item.value == selected} value={item.value}>
