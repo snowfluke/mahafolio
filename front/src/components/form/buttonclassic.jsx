d145c1b904ee116d17ea57e73bc18ce864792aae
Willy Setiawan <wilisetiawan29@gmail.com>
2022-09-15 08:15:07 +0700

    Edit components form

--- front/src/components/form/buttonclassic.jsx
+++ front/src/components/form/buttonclassic.jsx
@@ -3,10 +3,8 @@ function ButtonClassic({ title, action, alter = false }) {
     <button
       onClick={action}
       className={
-        "responsive-text font-semibold px-10 py-2 tracking-widest border-y-2 " +
-        (alter
-          ? "bg-white text-red-500 hover:bg-red-100 border-red-500"
-          : "border-green bg-green text-white hover:text-green hover:bg-white")
+        "responsive-text font-semibold px-10 py-2 tracking-widest border-y-2" +
+        (alter ? "bg-white text-red-500 hover:bg-red-100 border-red-500" : "border-green bg-green text-white hover:text-green hover:bg-white")
       }
     >
       {title}
--- front/src/components/form/dropdown.jsx
+++ front/src/components/form/dropdown.jsx
@@ -1,7 +1,7 @@
 function Dropdown({ items, ref, name = "", selected = "", onChange = () => false }) {
   return (
     <div class="dropdown relative">
-      <select className="px-4 py-2 shadow block w-full" ref={ref} onChange={onChange} name={name}>
+      <select className="responsive-text-xs px-4 py-2 shadow block" ref={ref} onChange={onChange} name={name}>
         <For each={items}>
           {(item) => (
             <option selected={item.value == selected} value={item.value}>
